abc339a7d4643c10244898a607c6f69e88362c0f
Peter <71720941+PioterDev@users.noreply.github.com>
2024-10-02 12:37:01 +0000

    Car

--- Car.java
+++ Car.java
@@ -1,16 +1,21 @@
 public class Car {
-    // Attributes
-    
-    // Constructor
-    
-    // Method to display car information
-    
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
     }
+    private String make;
+    private String model;
+    private int year;
+    // Attributes    
+    
+    public void displayInformation() {
+        System.out.printf("Marka: %s%nModel: %s%nRok: %d%n", make, model, year);
+    }    
+    
 
     public static void main(String[] args) {
-        // Create a Car object
-        
-        // Display car details
-        
+        Car c = new Car("Honda", "Civic", 1994);
+        c.displayInformation();        
     }
 }
--- main.cpp
+++ main.cpp
@@ -1,18 +1,39 @@
 // include the necessary libraries
 
-class Car {
+// class Car {
     //attributes
 
     // Constructor
 
     // Method to display car information
    
+// };
+
+#include <iostream>
+
+class Car {
+    private:
+        std::string make;
+        std::string model;
+        int year;
+    public:
+        Car(std:: string makE, std::string modeL, int yeaR) {
+            make = makE;
+            model = modeL;
+            year = yeaR;
+        }
+
+        void displayInfo() {
+            std::cout << "Marka: " << make << "\n";
+            std::cout << "Model: " << model << "\n";
+            std::cout << "Rok: " << year << std::endl;
+        }
 };
 
 int main() {
-    // Create a Car object
     
-    // Display car details
-    
+    Car c("Honda", "Civic", 1994);
+    c.displayInfo();
+
     return 0;
 }
