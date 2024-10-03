dbf420f6b204f532b1714e619c318f378394b4f2
PioterDev <71720941+PioterDev@users.noreply.github.com>
2024-10-04 00:07:29 +0200

    Car car

--- Car.java
+++ Car.java
@@ -1,21 +1,38 @@
+import java.time.Year;
+
 public class Car {
     public Car(String make, String model, int year) {
         this.make = make;
         this.model = model;
         this.year = year;
+        Car.currentYear = Year.now().getValue();
     }
     private String make;
     private String model;
     private int year;
-    // Attributes    
+    public static int currentYear;
     
-    public void displayInformation() {
+    public void displayInfo() {
         System.out.printf("Marka: %s%nModel: %s%nRok: %d%n", make, model, year);
     }    
     
+    public int getCarAge() {
+        return Car.currentYear - this.year;
+    }
+
+    public boolean isSameCar(Car otherCar) {
+        return  this.make.equals(otherCar.make) &&
+                this.model.equals(otherCar.model) &&
+                this.year == otherCar.year;
+    }
 
     public static void main(String[] args) {
-        Car c = new Car("Honda", "Civic", 1994);
-        c.displayInformation();        
+        Car c1 = new Car("Honda", "Civic", 1994);
+        c1.displayInfo();    
+        System.out.println(c1.getCarAge());
+        
+        Car c2 = new Car("Toyota", "Corolla", 2007);
+        c2.displayInfo();
+        System.out.println(c2.isSameCar(c1));
     }
 }
--- main.cpp
+++ main.cpp
@@ -1,15 +1,5 @@
-// include the necessary libraries
-
-// class Car {
-    //attributes
-
-    // Constructor
-
-    // Method to display car information
-   
-// };
-
 #include <iostream>
+#include <ctime>
 
 class Car {
     private:
@@ -17,23 +7,46 @@ class Car {
         std::string model;
         int year;
     public:
-        Car(std:: string makE, std::string modeL, int yeaR) {
-            make = makE;
-            model = modeL;
-            year = yeaR;
+        static int currentYear;
+        Car(const std::string make, const std::string model, const int year) {
+            this->make = make;
+            this->model = model;
+            this->year = year;
         }
 
-        void displayInfo() {
+        inline void displayInfo() {
             std::cout << "Marka: " << make << "\n";
             std::cout << "Model: " << model << "\n";
             std::cout << "Rok: " << year << std::endl;
         }
+
+        inline int getCarAge() {
+            return Car::currentYear - this->year;
+        }
+
+        inline bool isSameCar(Car* otherCar) {
+            return !this->make.compare(otherCar->make) &&
+                   !this->model.compare(otherCar->model) &&
+                    this->year == otherCar->year;
+        }
 };
 
+int Car::currentYear = 0;
+
 int main() {
-    
-    Car c("Honda", "Civic", 1994);
-    c.displayInfo();
+    time_t t;
+    time(&t);
+    struct tm* now = gmtime(&t);
+    Car::currentYear = now->tm_year + 1900;
+
+    Car c1("Honda", "Civic", 1994);
+    c1.displayInfo();
+    std::cout << c1.getCarAge() << "\n";
+
+    Car c2("Toyota", "Corolla", 2007);
+    c2.displayInfo();
+    std::cout << c2.getCarAge() << "\n";
+    std::cout << (c2.isSameCar(&c1) ? "Same" : "Different") << std::endl;
 
     return 0;
 }
